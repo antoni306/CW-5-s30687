cf8a3fac08b79df15d8d149bf55167b161366fa6
Antoni <s30687@pjwstk.edu.pl>
2025-04-09 18:41:39 +0200

    Created class Animal and Controller for Animal

---
+++ WebApplication1/WebApplication1/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public double Weight { get; set; }
+    public string HairColor { get; set; }
+
+    public Animal(int id, string name, string category, double weight, string hairColor)
+    {
+        Id = id;
+        Name = name;
+        Category = category;
+        Weight = weight;
+        HairColor = hairColor;
+    }
+}
