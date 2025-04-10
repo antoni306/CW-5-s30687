5eb5b46f90132588f58617689dd11f6cad3a16be
Antoni <s30687@pjwstk.edu.pl>
2025-04-10 10:11:43 +0200

    Renamed AnimalController to AnimalsController

---
+++ WebApplication1/WebApplication1/Controllers/AnimalsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class AnimalController : Controller
+{
+    private List<Animal> animals = 
+            [
+                new Animal(1,"Reks","York",1.7,"White"),
+                new Animal(2,"Princess","Pitbull",3.5,"Black"),
+                new Animal(3,"Perla","Husky",4.2,"Brown")
+            ];
+
+   
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        return Ok(animals);
+    }
+    [HttpGet]
+    [Route("{id}")]
+    public IActionResult GetAnimal(int id)
+    {
+        var animal = animals.FirstOrDefault(a => a.Id == id);
+        if (animal == null)
+            return NotFound();
+        return Ok(animal);
+    }
+    [HttpPost]
+    public IActionResult AddAnimal([FromBody] Animal animal)
+    {
+        int id = animals.Max(a => a.Id) + 1;
+        animal.Id = id;
+        
+        animals.Add(animal);
+        return Created($"/animals/{animal.Id}",animal);
+    }
+}
